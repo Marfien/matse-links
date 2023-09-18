9efa96de9cd3d77559b62ee12aeafd944e951b74
Marfien <marvin.sempa.haase@gmail.com>
2023-09-18 14:23:57 +0200

    --deploy

--- vue.config.ts
+++ vue.config.ts
@@ -1,3 +1,6 @@
+import * as process from "process";
+
+console.log(JSON.stringify(process.env))
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? "/matse-links/" : "/",
 };
