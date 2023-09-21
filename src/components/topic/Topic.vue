6b3781af97d065e8ec26c139a94cc55cfaae2290
Marfien <marvin.sempa.haase@gmail.com>
2023-09-21 13:28:13 +0200

    Added transition when topic changes --deploy

--- src/components/topic/Topic.vue
+++ src/components/topic/Topic.vue
@@ -1,24 +1,7 @@
 <script setup lang="ts">
 defineProps({
   name: String,
-})
-
-// onMounted(() => {
-//   for (let topic of document.getElementsByClassName("topic")) {
-//     let i = 0
-//     for (let subtopic of topic.getElementsByClassName("subtopic")) {
-//       let style = subtopic.style;
-//       if (i++ % 2 == 0) {
-//         style.backgroundColor = "var(--accent-color)";
-//       } else {
-//         style.backgroundImage = 'url("src/assets/waves/accent-color-top.svg")';
-//         style.backgroundRepeat = "no-repeat";
-//         style.backgroundSize = "100% auto";
-//         style.backgroundPositionY = "-5rem";
-//       }
-//     }
-//   }
-// });
+});
 </script>
 
 <template>
@@ -29,15 +12,10 @@ defineProps({
 
 <style scoped>
 .topic {
-  background-color: var(--topic-background-color);
-  background-image: url("/src/assets/waves/navbar-dark.svg");
-  background-repeat: no-repeat;
-  background-size: auto calc(100% + 2px);
-  width: calc(100% - 50px);
+  width: calc(100% - 50px - 1rem);
   background-position-y: center;
   background-position-x: -20px;
-  height: 100vh;
-  padding-left: 50px;
+  height: 100%;
   display: flex;
   gap: 5rem;
   flex-direction: column;
